b969e15641490d69f62d2b1e05799e6b415ff51a
andre <andre.vasquez@sogeti.com>
2022-06-14 13:47:50 -0500

    created aboutAPI and deleted un needed files

--- api/routes/about.js
+++ api/routes/about.js
@@ -4,6 +4,7 @@ const router=express.Router();
 const Model = require('../models/about');
 module.exports=router
 
+//get all
 router.get("/about",async (req,res)=>{
    try{
        const response = await Model.find();
@@ -14,6 +15,7 @@ router.get("/about",async (req,res)=>{
    }
 })
 
+//get by id
 router.get('/about/:id', async (req,res)=>{
     try{
         const response = await Model.findById(req.params.id);
@@ -24,6 +26,7 @@ router.get('/about/:id', async (req,res)=>{
     }
 })
 
+//post method
 router.post("/add-about", async(req,res)=>{
     const aboutData = new Model({
         title: req.body.title,
@@ -33,6 +36,32 @@ router.post("/add-about", async(req,res)=>{
     try{
         const response = await aboutData.save();
         res.status(200).json(response)
+    }
+    catch(error){
+        res.status(400).json({message: error.message})
+    }
+})
+
+//update method
+router.patch('/update-about', async (req,res)=>{
+    try{
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = {new:true};
+
+        const response = await Model.findByIdAndUpdate(id,updatedData,options);
+    }
+    catch(error){
+        res.status(400).json({message:error.message})
+    }
+})
+
+//delete
+router.delete('/delete-about', async (req,res)=>{
+    try{
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id);
+        res.send(`Document with ${data.name} has been deleted..`)
     }
     catch(error){
         res.status(400).json({message: error.message})
